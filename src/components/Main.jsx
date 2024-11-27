a8132207f6a003319c7d2714655309bfb4025dc5
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-27 17:01:28 +0100

    ADD resto esercizio con bootstrap

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,35 @@
-
 export default function Main() {
   return (
-    <div className="card">
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+    <div className="container mx-auto">
+      <div className="row justify-content-center">
+        <div className="col-5 p-3">
+          <div className="card">
+            <img src="public\th.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Titolo del post</h5>
+              <p className="card-text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione tempora, magni provident aliquid, nostrum sapiente nihil soluta autem aspernatur esse ipsa illo. Aut, officia sunt. Est nihil provident quaerat ad.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Leggi di più
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-5 p-3">
+          <div className="card">
+            <img src="public\th.jpg" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">Titolo del post</h5>
+              <p className="card-text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione tempora, magni provident aliquid, nostrum sapiente nihil soluta autem aspernatur esse ipsa illo. Aut, officia sunt. Est nihil provident quaerat ad.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Leggi di più
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
